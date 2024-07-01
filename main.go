c7f6b98059478c71fc76ed29add1d67e94ff1507
Riya Arora <ariya24396@gmail.com>
2024-07-01 13:39:00 +0530

    delete main.go

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
-package main
 
-import (
-	"fmt"
-	"os"
-	"github.com/riyaarora65/cron_parser/app/services"
-)
-
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: cron-expander \"<cron_string>\"")
-		return
-	}
-
-	cronString := os.Args[1]
-	cronService := services.NewCronService()
-	cronService.ParseAndPrint(cronString)
-}
